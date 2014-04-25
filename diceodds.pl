b1f6ae57c0f7c18f2823b734402ab4aaecf661b7
Ricardo Urbina <rurbina@users.noreply.github.com>
2014-04-25 18:12:39 -0500

    diceodds.pl es una herramienta para graficar la probabilidad de resultados de diferentes tiradas de dados

---
+++ diceodds.pl
@@ -0,0 +1,49 @@
+#! /usr/local/bin/perl
+use common::sense;
+use Data::Dumper qw(Dumper);
+
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 2;
+
+foreach my $dice_spec ( @ARGV ) {
+
+	my ($count,$sides) = split /d/, $dice_spec;
+
+	my %values = &valuetize($sides, $count);
+
+	foreach my $k ( sort { $a <=> $b } keys %values ) {
+		say "| $k | " . ( "x" x $values{$k} ) . " |";
+	}
+
+}
+
+exit;
+
+sub valuetize {
+
+	my ($sides, $count) = @_;
+
+	my %values;
+
+	my @rolls;
+
+	for my $index ( 1 .. ($sides ** $count) ) {
+		my @roll;
+		my $superindex = $index - 1;
+		for my $die ( 1 .. $count ) {
+			push @roll, ( $superindex % $sides ) + 1;
+			$superindex = int ( $superindex / $sides );
+		}
+		push @rolls, \@roll;
+	}
+
+	foreach my $r ( @rolls ) {
+		my $sum;
+		map { $sum += $_ } @$r;
+		$values{$sum}++;
+	}
+
+	return %values;
+
+}
+
